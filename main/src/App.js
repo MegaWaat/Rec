4e79e440527ca0c81b6c0c77af0ee0c1aa33c469
Kevin C.Camargo <kevincavalheiro@hotmail.com>
2020-11-23 18:10:05 -0300

    server js

--- main/src/App.js
+++ main/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
+//import React from 'react'
 import './App.css'
+import React, {Component} from 'react'
+
 /* import Login from './pages/Login'
 import { Link } from 'react-router-dom' */
 import Hello from './pages/Hello/Hello'
@@ -7,10 +9,34 @@ import Hello from './pages/Hello/Hello'
 export default (props) =>(
   
 <div>
-  
-
-    <Hello></Hello>
-          
+    <Hello></Hello>        
 </div>);
 
 
+
+/* class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { posts: [] };
+
+    fetch('http://localhost:3000/posts')
+      .then(response => response.json())
+      .then(posts => (this.setState({posts}))
+  }
+  render() {
+    return (<div>
+      Hello World
+      <ul>
+        {this.state.posts.map(post => <li>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </li>}
+      </ul>
+    </div>);
+  }
+}
+
+ReactDOM.render(
+  <Posts />,
+  mountNode
+); */
