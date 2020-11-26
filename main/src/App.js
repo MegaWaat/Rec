9ec752a96da4747a8526706abce45abf76a654c3
Kevin C.Camargo <kevincavalheiro@hotmail.com>
2020-11-26 17:49:34 -0300

    conexao com vd

--- main/src/App.js
+++ main/src/App.js
@@ -1,42 +1,44 @@
-//import React from 'react'
 import './App.css'
-import React, {Component} from 'react'
-
-/* import Login from './pages/Login'
-import { Link } from 'react-router-dom' */
+import React from 'react'
 import Hello from './pages/Hello/Hello'
+import { response } from "express"; //
+/* const express=require('express');
+const bodyParser=require('body-parser');
+const mysql=require('mysql');
+const app=express();const urlencodeParser=bodyParser.urlencoded({extended:false});
+const sql=mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'u1n9e1',
+  port:3306
+  
+});
+sql.query("use nodejs");
+//Templates
+app.get("/inserir",function(req,res){res.render("inserir");});
+app.post("/controllerForm",urlencodeParser,function(req,res){
+    sql.query("insert into user values (?,?,?)",[req.body.id,req.body.name,req.body.age]);
+    res.render('controllerForm',{name:req.body.name});
+});
+
+ */
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const connection = mysql.createPool({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'u1n9e1',
+  database : 'nodejs'
+});
+
 
 export default (props) =>(
+  
   
 <div>
     <Hello></Hello>        
 </div>);
 
-
-
-/* class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { posts: [] };
-
-    fetch('http://localhost:3000/posts')
-      .then(response => response.json())
-      .then(posts => (this.setState({posts}))
-  }
-  render() {
-    return (<div>
-      Hello World
-      <ul>
-        {this.state.posts.map(post => <li>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </li>}
-      </ul>
-    </div>);
-  }
-}
-
-ReactDOM.render(
-  <Posts />,
-  mountNode
-); */
